be1bce0bdbb2ae426ccf718b345c5ca8752fbe22
RhAnjiE <irhanjie@gmail.com>
2024-03-01 23:44:22 +0100

    Load sprite in image class (do not blame me pls)

--- src/engine/types/image.ts
+++ src/engine/types/image.ts
@@ -2,11 +2,16 @@ import {Type} from './index'
 import {Engine} from '../index'
 import {ImageDefinition} from '../../fileFormats/cnv/types'
 import {NotImplementedError} from '../../utils'
+import {getIMGFile} from "../../filesLoader";
+import * as PIXI from "pixi.js";
+import {Sprite} from "pixi.js";
 
 export class Image extends Type<ImageDefinition> {
     private visible: boolean
     private opacity: number = 1
     private priority: number
+
+    private sprite: Sprite
 
     private x: number = 0
     private y: number = 0
@@ -15,6 +20,8 @@ export class Image extends Type<ImageDefinition> {
         super(engine, definition)
         this.visible = definition.VISIBLE
         this.priority = definition.PRIORITY
+
+        this.Load()
     }
 
     SETOPACITY(opacity: number) {
@@ -24,6 +31,25 @@ export class Image extends Type<ImageDefinition> {
     MOVE(xOffset: number, yOffset: number) {
         this.x += xOffset
         this.y += yOffset
+    }
+
+    private Load() { //'DANE/ReksioUfo/PRZYGODA/s1_0_intro1/gwiazdy.img'
+        console.log(`Loading file: ${this.definition.FILENAME}`)
+        
+        getIMGFile(this.definition.FILENAME).then((image) => {
+            const baseTexture = PIXI.BaseTexture.fromBuffer(
+                new Uint8Array(image.bytes),
+                image.header.width,
+                image.header.height
+            )
+
+            const texture = new PIXI.Texture(baseTexture)
+            this.sprite = new PIXI.Sprite(texture)
+
+            this.SETPOSITION(0, 0)
+            this.sprite.visible = this.definition.VISIBLE
+            this.engine.app.stage.addChild(this.sprite);
+        });
     }
 
     SETPOSITION(x: number, y: number) {
@@ -40,11 +66,11 @@ export class Image extends Type<ImageDefinition> {
     }
 
     SHOW() {
-        this.visible = true
+        this.sprite.visible = true
     }
 
     HIDE() {
-        this.visible = false
+        this.sprite.visible = false
     }
 
     GETPOSITIONY() {
