b1122cfa1068242c7fa5e28dfc4872083dc76662
RhAnjiE <irhanjie@gmail.com>
2024-03-06 02:01:33 +0100

    Add temporary display of images in animo class

--- src/engine/types/animo.ts
+++ src/engine/types/animo.ts
@@ -2,14 +2,20 @@ import {Type} from './index'
 import {AnimoDefinition, callbacks} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
 import {NotImplementedError} from '../../utils'
-import {AnimatedSprite} from 'pixi.js'
+import {AnimatedSprite, Sprite} from 'pixi.js'
 import {FileNotFoundError} from '../../filesLoader'
+import {ANN} from '../../fileFormats/ann'
+import * as PIXI from 'pixi.js'
 
 export class Animo extends Type<AnimoDefinition> {
     private priority: number
     private visible: boolean
+    private isPlay: boolean = false
+    private currentFrame: number = 0
 
-    private animatedSprite: AnimatedSprite | null = null
+    //private animatedSprite: AnimatedSprite | null = null
+    private rawAnn: ANN | null = null
+    private sprite: Sprite | null = null
 
     private readonly onFinished: callbacks<string>
 
@@ -22,9 +28,11 @@ export class Animo extends Type<AnimoDefinition> {
     }
 
     async init() {
-        const animation = await this.loadAnimation()
+        this.rawAnn = await this.loadAnimation()
 
-        //this.initAnimatedSprite(animation);
+        //TODO: Use Image class maybe (to check)
+
+        this.initAnimatedSprite()
 
         if (this.definition.ONINIT) {
             this.engine.executeCallback(this, this.definition.ONINIT)
@@ -32,15 +40,13 @@ export class Animo extends Type<AnimoDefinition> {
     }
 
     destroy() {
-        if (this.animatedSprite != null) {
-            this.animatedSprite.destroy()
-        }
+        if (this.sprite === null) return
+
+        this.sprite.destroy()
     }
 
     tick(delta: number) {
-        if (!this.visible) {
-            return
-        }
+        if (!this.visible || !this.isPlay) return
 
         this.ONTICK()
     }
@@ -54,14 +60,32 @@ export class Animo extends Type<AnimoDefinition> {
     }
 
     private initAnimatedSprite() {
+        if (this.rawAnn === null) return
+
         this.SETPRIORITY(this.definition.PRIORITY)
         //this.animatedSprite.visible = this.definition.VISIBLE
-        //this.engine.addToStage(this.animatedSprite)
+
+        const ufoBaseTexture = PIXI.BaseTexture.fromBuffer(
+            new Uint8Array(this.rawAnn.images[0]),
+            this.rawAnn.annImages[0].width,
+            this.rawAnn.annImages[0].height
+        )
+
+        const ufoTexture = new PIXI.Texture(ufoBaseTexture)
+        this.sprite = new PIXI.Sprite(ufoTexture)
+        this.engine.addToStage(this.sprite)
 
         console.debug(`File ${this.definition.FILENAME} loaded successfully!`)
     }
 
     ONTICK() {
+        if (this.rawAnn === null || this.sprite === null) return
+
+        const eventFrame = this.rawAnn.events[0].frames[this.currentFrame]
+        this.sprite.x = this.rawAnn.annImages[0].positionX + eventFrame.positionX
+        this.sprite.y = this.rawAnn.annImages[0].positionY + eventFrame.positionY
+        this.currentFrame = (this.currentFrame + 1) % this.rawAnn.events[0].frames.length
+
         const index = Math.floor(Math.random() * 5)
 
         if (this.onFinished && this.onFinished.parametrized.has(index.toString())){
@@ -70,80 +94,106 @@ export class Animo extends Type<AnimoDefinition> {
     }
 
     PLAY(name: string) {
-        throw new NotImplementedError()
+        this.isPlay = true
+        this.currentFrame = 0
     }
+
+    STOP(arg: boolean) {
+        this.isPlay = false
+        this.currentFrame = 0
+    }
+
+    PAUSE() {
+        this.isPlay = false
+    }
+
+    RESUME() {
+        this.isPlay = true
+    }
+
     SETFRAME(frame: number) {
         throw new NotImplementedError()
     }
+
     SETFPS(fps: number) {
         throw new NotImplementedError()
     }
-    SETPOSITION(x: number, y: number) {
-        throw new NotImplementedError()
-    }
-    SETPRIORITY(priority: number) {
-        this.priority = priority
-    }
-    SETASBUTTON(arg1: boolean, arg2: boolean) {
-        throw new NotImplementedError()
-    }
-    GETCENTERX(): number {
-        throw new NotImplementedError()
-    }
-    GETCENTERY(): number {
-        throw new NotImplementedError()
-    }
-    GETPOSITIONY(): number {
-        throw new NotImplementedError()
-    }
-    GETPOSITIONX(): number {
-        throw new NotImplementedError()
-    }
-    GETFRAMENAME(): string {
-        throw new NotImplementedError()
-    }
-    GETMAXWIDTH(): number {
-        throw new NotImplementedError()
-    }
-    GETNOFINEVENT(): string {
-        throw new NotImplementedError()
-    }
-    GETEVENTNAME(): string {
-        throw new NotImplementedError()
-    }
-    GETFRAME(): number {
-        throw new NotImplementedError()
-    }
-    GETCURRFRAMEPOSX(): number {
-        throw new NotImplementedError()
-    }
-    GETCURRFRAMEPOSY(): number {
-        throw new NotImplementedError()
-    }
+
     SHOW() {
         this.visible = true
     }
     HIDE() {
         this.visible = false
     }
-    STOP(arg: boolean) {
+
+    SETPOSITION(x: number, y: number) {
         throw new NotImplementedError()
     }
-    PAUSE() {
+
+    SETPRIORITY(priority: number) {
+        this.priority = priority
+    }
+
+    SETASBUTTON(arg1: boolean, arg2: boolean) {
         throw new NotImplementedError()
     }
-    RESUME() {
+
+    GETCENTERX(): number {
         throw new NotImplementedError()
     }
+
+    GETCENTERY(): number {
+        throw new NotImplementedError()
+    }
+
+    GETPOSITIONY(): number {
+        throw new NotImplementedError()
+    }
+
+    GETPOSITIONX(): number {
+        throw new NotImplementedError()
+    }
+
+    GETFRAMENAME(): string {
+        throw new NotImplementedError()
+    }
+
+    GETMAXWIDTH(): number {
+        throw new NotImplementedError()
+    }
+
+    GETNOFINEVENT(): string {
+        throw new NotImplementedError()
+    }
+
+    GETEVENTNAME(): string {
+        throw new NotImplementedError()
+    }
+
+    GETFRAME(): number {
+        throw new NotImplementedError()
+    }
+
+    GETCURRFRAMEPOSX(): number {
+        throw new NotImplementedError()
+    }
+
+    GETCURRFRAMEPOSY(): number {
+        throw new NotImplementedError()
+    }
+
     MOVE(offsetX: number, offsetY: number) {
         throw new NotImplementedError()
     }
+
     CLONE(arg: number) {
         throw new NotImplementedError()
     }
+
     ISPLAYING(animName: string) {
         throw new NotImplementedError()
     }
+
     ISNEAR(objectName: string, arg: number) {
         throw new NotImplementedError()
     }
--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,8 @@ const main = async () => {
     const engine = new Engine(app)
     await engine.init()
 
-    // Ufo
-    const ufo = await engine.fileLoader.getANNFile('DANE/ReksioUfo/PRZYGODA/s1_0_intro1/ufo.ann')
-    const ufoBaseTexture = PIXI.BaseTexture.fromBuffer(new Uint8Array(ufo.images[0]), ufo.annImages[0].width, ufo.annImages[0].height)
-    const ufoTexture = new PIXI.Texture(ufoBaseTexture)
-
-    const ufoSprite = new PIXI.Sprite(ufoTexture)
-    ufoSprite.x = 0
-    ufoSprite.y = 0
-    app.stage.addChild(ufoSprite)
-
-    let currentFrame = 0
     app.ticker.add(delta => {
         engine.tick(delta)
-
-        const eventFrame = ufo.events[0].frames[currentFrame]
-        ufoSprite.x = ufo.annImages[0].positionX + eventFrame.positionX
-        ufoSprite.y = ufo.annImages[0].positionY + eventFrame.positionY
-        currentFrame = (currentFrame + 1) % ufo.events[0].frames.length
     })
 }
 
