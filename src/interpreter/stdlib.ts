0bb83720a051d6f819af961c959e3caa2fa92d2d
Adam Zambrzycki <adam@zambrzycki.net>
2024-03-11 00:14:55 +0100

    RANDOM^GET support single parameter call

--- src/interpreter/stdlib.ts
+++ src/interpreter/stdlib.ts
@@ -1,7 +1,10 @@
 export const libraries = {
     RANDOM: {
         // min - included, max - excluded
-        GET: (min: number, max: number) => {
+        GET: (min: number, max?: number) => {
+            if (max === undefined) {
+                return libraries.RANDOM.GET(0, min)
+            }
             return Math.floor(Math.random() * (max - min)) + min
         }
     }
