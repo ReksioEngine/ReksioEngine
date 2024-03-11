6269d4a06620556594e37090eadd260db84d8c84
Adam Zambrzycki <adam@zambrzycki.net>
2024-03-11 21:20:51 +0100

    Implement keyboard support

--- src/engine/types/keyboard.ts
+++ src/engine/types/keyboard.ts
@@ -1,15 +1,54 @@
 import {Type} from './index'
 import {KeyboardDefinition, MusicDefinition} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
-import {NotImplementedError} from '../../utils'
+
+const keysMapping = {
+    ArrowLeft: 'LEFT',
+    ArrowRight: 'RIGHT',
+    ArrowUp: 'UP',
+    ArrowDown: 'DOWN',
+    Space: 'SPACE'
+} as any
 
 export class Keyboard extends Type<KeyboardDefinition> {
+    private keysState = new Map<string, boolean>()
+
+    private readonly onKeyDownCallback: (event: KeyboardEvent) => void
+    private readonly onKeyUpCallback: (event: KeyboardEvent) => void
+
     constructor(engine: Engine, definition: MusicDefinition) {
         super(engine, definition)
+        this.onKeyDownCallback = this.onKeyDown.bind(this)
+        this.onKeyUpCallback = this.onKeyUp.bind(this)
     }
 
-    // UP, DOWN, RIGHT, LEFT
+    ready() {
+        window.addEventListener('keydown', this.onKeyDownCallback)
+        window.addEventListener('keyup', this.onKeyUpCallback)
+    }
+
+    destroy() {
+        window.removeEventListener('keydown', this.onKeyDownCallback)
+        window.removeEventListener('keyup', this.onKeyUpCallback)
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        this.setKeyState(event.code, true)
+    }
+
+    onKeyUp(event: KeyboardEvent) {
+        this.setKeyState(event.code, false)
+    }
+
+    setKeyState(keyCode: string, value: boolean) {
+        const mapped = keysMapping[keyCode]
+        if (!mapped) {
+            throw new Error(`Unsupported keyboard key code ${keyCode}`)
+        }
+        this.keysState.set(mapped, value)
+    }
+
     ISKEYDOWN(keyName: string) {
-        throw new NotImplementedError()
+        return this.keysState.get(keyName) ?? false
     }
 }
