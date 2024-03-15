c05c1de5a1205f9208d486f0be6a243c2b7090a1
Adam Zambrzycki <adam@zambrzycki.net>
2024-03-15 14:52:51 +0100

    Make RANDOM^GET accept string arguments

--- src/interpreter/stdlib.ts
+++ src/interpreter/stdlib.ts
@@ -1,10 +1,20 @@
 export const libraries = {
     RANDOM: {
         // min - included, max - excluded
-        GET: (min: number, max?: number) => {
+        GET: (min: number | string, max?: number | string) => {
+            // Min and Max can be a string containing a number.
+            // Probably behaviour's constant arguments can only be a string
+            if (typeof min === 'string') {
+                min = parseInt(min)
+            }
+            if (typeof max === 'string') {
+                max = parseInt(max)
+            }
+
             if (max === undefined) {
                 return libraries.RANDOM.GET(0, min)
             }
+
             return Math.floor(Math.random() * (max - min)) + min
         }
     }
