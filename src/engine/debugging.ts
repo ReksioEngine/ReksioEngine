c7d838f07e64c6d49e94506f5aff1df2166d4997
Adam Zambrzycki <adam@zambrzycki.net>
2024-06-20 01:30:32 +0200

    Mark non-existing scenes files in debug selector

--- src/engine/debugging.ts
+++ src/engine/debugging.ts
@@ -9,10 +9,18 @@ export const setupDebugScene = (engine: Engine) => {
 
         const episode: Episode = Object.values(engine.globalScope).find((object: Type<any>) => object.definition.TYPE === 'EPISODE')
         const container: any = document.querySelector('#sceneSelector')!
-        for (const scene of episode.definition.SCENES) {
+        for (const sceneName of episode.definition.SCENES) {
+            const scene = Object.values(engine.globalScope).find((object: Type<any>) => {
+                return object.definition.TYPE === 'SCENE' && object.definition.NAME === sceneName
+            })
+
+            const sceneDefPath = scene.getRelativePath(`${sceneName}.cnv`)
+            const canGoTo = engine.fileLoader.getFilesListing().includes(sceneDefPath.toLowerCase())
+
             const option = document.createElement('option')
-            option.value = scene
-            option.text = scene
+            option.value = sceneName
+            option.text = sceneName
+            option.disabled = !canGoTo
             container.appendChild(option)
         }
         const button = document.querySelector('#changeScene')!
