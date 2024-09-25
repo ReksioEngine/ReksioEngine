d287a6efb148779d9ac5edcc5e497697a6d8a8ae
Adam Zambrzycki <adam@zambrzycki.net>
2024-09-26 00:39:30 +0200

    Make ARRAY^LOADINI set value to empty array instead of null if no value in save file

--- src/engine/types/array.ts
+++ src/engine/types/array.ts
@@ -78,7 +78,7 @@ export class ArrayObject extends ValueType<ArrayDefinition> {
     }
 
     LOADINI() {
-        this.value = this.getFromINI()
+        this.value = this.getFromINI() ?? []
     }
 
     MSGBOX() {
