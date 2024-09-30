f85aa5537f2233d6ffd11c6d4c6dd704327565be
Adam Zambrzycki <adam@zambrzycki.net>
2024-10-01 01:20:14 +0200

    Implement VECTOR^NORMALIZE and VECTOR^REFLECT

--- src/engine/types/vector.ts
+++ src/engine/types/vector.ts
@@ -1,6 +1,7 @@
 import {ValueType} from './index'
 import {VectorDefinition} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
+import {assert} from '../../errors'
 
 export class Vector extends ValueType<VectorDefinition> {
     constructor(engine: Engine, definition: VectorDefinition) {
@@ -22,5 +23,27 @@ export class Vector extends ValueType<VectorDefinition> {
 
     GET(index: number) {
         return this.value[index]
+    }
+
+    NORMALIZE() {
+        const magnitude: number = Math.sqrt(this.value.reduce((sum: number, val: number) => sum + val * val, 0))
+        assert(magnitude !== 0, 'Cannot normalize a zero vector')
+
+        this.value = this.value.map((val: number) => val / magnitude)
+    }
+
+    REFLECT(vector: number[], normal: number[]): void {
+        assert(
+            this.value.length === normal.length && this.value.length === vector.length,
+            'Vector and normal must have the same dimensionality'
+        )
+
+        // Calculate the dot product of the vector and the normal vector
+        const dotProduct: number = vector.reduce((sum: number, val: number, idx: number) => sum + val * normal[idx], 0)
+
+        // Calculate the reflection: v - 2 * (v . n) * n
+        this.value = vector.map((val: number, idx: number) =>
+            val - 2 * dotProduct * normal[idx]
+        )
     }
 }
