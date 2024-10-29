1e5eaf0ba70587b6eaa11f541d6b58461e5c1d8c
Adam Zambrzycki <adam@zambrzycki.net>
2024-10-30 00:45:01 +0100

    Add printing condition details on error during comparison

--- src/engine/types/condition.ts
+++ src/engine/types/condition.ts
@@ -3,6 +3,7 @@ import {ConditionDefinition} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
 import {InterruptScriptExecution} from '../../interpreter/evaluator'
 import {Compare} from '../../types'
+import {UnexpectedError} from '../../errors'
 
 export class Condition extends Type<ConditionDefinition> {
     constructor(engine: Engine, definition: ConditionDefinition) {
@@ -32,25 +33,40 @@ export class Condition extends Type<ConditionDefinition> {
         const operand2 = this.engine.executeCallback(null, this.definition.OPERAND2)
 
         let result
-        switch (this.definition.OPERATOR) {
-        case 'EQUAL':
-            result = Compare.Equal(operand1, operand2)
-            break
-        case 'NOTEQUAL':
-            result = Compare.NotEqual(operand1, operand2)
-            break
-        case 'LESS':
-            result = Compare.Less(operand1, operand2)
-            break
-        case 'GREATER':
-            result = Compare.Greater(operand1, operand2)
-            break
-        case 'LESSEQUAL':
-            result = Compare.LessOrEqual(operand1, operand2)
-            break
-        case 'GREATEREQUAL':
-            result = Compare.GreaterOrEqual(operand1, operand2)
-            break
+
+        try {
+            switch (this.definition.OPERATOR) {
+            case 'EQUAL':
+                result = Compare.Equal(operand1, operand2)
+                break
+            case 'NOTEQUAL':
+                result = Compare.NotEqual(operand1, operand2)
+                break
+            case 'LESS':
+                result = Compare.Less(operand1, operand2)
+                break
+            case 'GREATER':
+                result = Compare.Greater(operand1, operand2)
+                break
+            case 'LESSEQUAL':
+                result = Compare.LessOrEqual(operand1, operand2)
+                break
+            case 'GREATEREQUAL':
+                result = Compare.GreaterOrEqual(operand1, operand2)
+                break
+            }
+        } catch (err) {
+            if (err instanceof UnexpectedError) {
+                console.error(
+                    'Condition details\n' +
+                    '\n' +
+                    '%cCondition:%c%O\n%cOperand1:%c%O\n%cOperand2:%c%O\n',
+                    'font-weight: bold', 'font-weight: inherit', this,
+                    'font-weight: bold', 'font-weight: inherit', operand1,
+                    'font-weight: bold', 'font-weight: inherit', operand2,
+                )
+            }
+            throw err
         }
 
         if (result) {
