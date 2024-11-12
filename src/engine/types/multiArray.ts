96a4a7f7a5ecd11cc758a2a59c1c9f2b1af87776
Adam Zambrzycki <adam@zambrzycki.net>
2024-11-12 14:08:45 +0100

    Add missing method() decorators

--- src/engine/types/multiArray.ts
+++ src/engine/types/multiArray.ts
@@ -2,16 +2,19 @@ import {ValueType} from './index'
 import {MultiArrayDefinition} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
 import {NotImplementedError} from '../../errors'
+import {method} from '../../types'
 
 export class MultiArray extends ValueType<MultiArrayDefinition> {
     constructor(engine: Engine, definition: MultiArrayDefinition) {
         super(engine, definition, [])
     }
 
+    @method()
     SET(arg1: number, arg2: number, arg3: any) {
         throw new NotImplementedError()
     }
 
+    @method()
     GET(arg1: number, arg2: number) {
         throw new NotImplementedError()
     }
