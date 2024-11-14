bea6f7c9dcbc76e0d70b7a06db131eaa2d847a81
x-Yoshida <69041080+x-Yoshida@users.noreply.github.com>
2024-11-14 01:20:22 +0100

    Did implementation for 2 dimensions

--- src/engine/types/multiArray.ts
+++ src/engine/types/multiArray.ts
@@ -2,20 +2,39 @@ import {ValueType} from './index'
 import {MultiArrayDefinition} from '../../fileFormats/cnv/types'
 import {Engine} from '../index'
 import {NotImplementedError} from '../../errors'
+import {assert} from '../../errors'
 import {method} from '../../types'
 
+const generateMessage = (action: string, x: number, y: number, culprit: string, value: any[]) => {
+    
+    return `Tried to ${action} an element at an possition (y: ${y}, x: ${x}) that is outside the bounds of the array (${culprit} length is ${value.length})`
+}
+//Only 2 dimensional for now at least
 export class MultiArray extends ValueType<MultiArrayDefinition> {
     constructor(engine: Engine, definition: MultiArrayDefinition) {
         super(engine, definition, [])
     }
 
     @method()
-    SET(arg1: number, arg2: number, arg3: any) {
-        throw new NotImplementedError()
+    SET(y: number, x: number, value: any) {
+        // console.log(this.value)
+        if(y>=this.value.length){
+            this.value.push([value])
+            return
+        }
+        if(x>=this.value[y].length){
+            this.value[y].push(value)
+            return
+        }
+        this.value[y][x] = value
+        // throw new NotImplementedError()
     }
 
     @method()
-    GET(arg1: number, arg2: number) {
-        throw new NotImplementedError()
+    GET(y: number, x: number) {
+        assert(y < this.value.length, generateMessage('get', y, x, 'y', this.value))
+        assert(x < this.value[y].length, generateMessage('get', y, x, 'x', this.value[y]))
+        return this.value[y][x]
+        // throw new NotImplementedError()
     }
 }
