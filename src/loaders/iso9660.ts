f77b2d49261b6cc8cdaf55dd8176ce99692abac0
Adam Zambrzycki <adam@zambrzycki.net>
2025-02-09 20:27:29 +0100

    Clean up iso9660 reader a bit

--- src/loaders/iso9660.ts
+++ src/loaders/iso9660.ts
@@ -1,29 +1,32 @@
 import { BinaryBuffer } from '../fileFormats/utils'
 
+const SECTOR_SIZE = 2048
+const JULIET_SECTOR = 17
+
 export type FileEntry = {
-    offset: number
+    name: string
+    location: number
     size: number
+    flags: number
 }
 
 export class Iso9660Reader {
     private file: File
     private filesMapping: Map<string, FileEntry> = new Map()
 
+    private decoder = new TextDecoder('utf-16le')
+
     constructor(file: File) {
         this.file = file
     }
 
-    private readAt(offset: number, length: number): Promise<ArrayBuffer> {
-        return new Promise((resolve, reject) => {
-            const blob = this.file.slice(offset, offset + length)
-            const reader = new FileReader()
-            reader.onload = (event) => {
-                if (event.target === null) {
-                    reject()
-                    return
-                }
-                resolve(event.target.result as ArrayBuffer)
-            }
+    private async readAt(offset: number, length: number): Promise<ArrayBuffer> {
+        const blob = this.file.slice(offset, offset + length)
+        const reader = new FileReader()
+
+        return new Promise<ArrayBuffer>((resolve, reject) => {
+            reader.onload = () => resolve(reader.result as ArrayBuffer)
+            reader.onerror = () => reject(reader.error)
             reader.readAsArrayBuffer(blob)
         })
     }
@@ -42,47 +45,48 @@ export class Iso9660Reader {
             swappedBuffer[i + 1] = view.getUint8(i) // Low byte
         }
 
-        const decoder = new TextDecoder('utf-16le')
-        return decoder.decode(swappedBuffer)
+        return this.decoder.decode(swappedBuffer)
+    }
+
+    private readFileName(directory: BinaryBuffer, identifierLength: number): string {
+        if (identifierLength === 1) {
+            const charCode = directory.getUint8()
+            return charCode === 0 ? '.' : charCode === 1 ? '..' : ''
+        } else {
+            const name = this.decodeUTF16BE(directory.read(identifierLength))
+            // Remove version (if present in name)
+            const semicolonIndex = name.indexOf(';')
+            return semicolonIndex > -1 ? name.substring(0, semicolonIndex) : name
+        }
     }
 
     private async processDirectory(position: number, length: number, path: Array<string>): Promise<void> {
-        const directory = await this.bufferAt(position * 2048, length)
+        const directory = await this.bufferAt(position * SECTOR_SIZE, length)
         while (directory.offset < length) {
             const startOffset = directory.offset
 
             const directoryRecordLength = directory.getUint8() // Length of Directory Record
             if (directoryRecordLength === 0) {
                 // Didn't fit in the sector
-                directory.skip(2048 - (startOffset % 2048) - 1)
+                directory.skip(SECTOR_SIZE - (startOffset % SECTOR_SIZE) - 1)
                 continue
             }
 
-            directory.getUint8() // Extended Attribute Record length.
-            const locationOfExtent = directory.getUint32() // Location of extent (LBA) in both-endian format.
-            directory.getUint32()
-            const dataLength = directory.getUint32() // Data length (size of extent) in both-endian format.
-            directory.getUint32()
-            directory.read(7) // Recording date and time.
-            const flags = directory.getUint8() //  	File flags.
-            directory.getUint8() // File unit size for files recorded in interleaved mode, zero otherwise.
-            directory.getUint8() // Interleave gap size for files recorded in interleaved mode, zero otherwise.
-            directory.getUint16() // Volume sequence number - the volume that this extent is recorded on, in 16 bit both-endian format.
+            directory.getUint8()
+            const locationOfExtent = directory.getUint32() // LE
+            directory.getUint32() // BE
+            const dataLength = directory.getUint32() // LE
+            directory.getUint32() // BE
+            directory.read(7)
+            const flags = directory.getUint8() // File flags.
+            directory.getUint8()
+            directory.getUint8()
+            directory.getUint16()
             directory.getUint16()
             const identifierLength = directory.getUint8()
 
-            // Get filename
-            let name = ''
-            if (identifierLength == 1) {
-                const value = directory.getUint8()
-                if (value === 0) {
-                    name = '.'
-                } else if (value === 1) {
-                    name = '..'
-                }
-            } else {
-                name = this.decodeUTF16BE(directory.read(identifierLength))
-            }
+            // Read filename
+            const name = this.readFileName(directory, identifierLength)
 
             // Padding
             if (identifierLength % 2 === 0) {
@@ -90,41 +94,38 @@ export class Iso9660Reader {
             }
 
             // Some special space
-            const restSize = directoryRecordLength - (directory.offset - startOffset)
-            directory.skip(restSize)
+            directory.skip(directoryRecordLength - (directory.offset - startOffset))
 
             if (name == '.' || name == '..') {
                 continue
             }
 
             // Do something with data
-            const isDirectory = (flags & 2) != 0
-            if (!isDirectory) {
-                name = name.substring(0, name.indexOf(';')) // Remove version
-            }
-
             const fullPathParts = [...path, name]
             const fullPath = fullPathParts.join('/').toLowerCase()
 
+            const isDirectory = (flags & 2) != 0
             if (isDirectory) {
                 await this.processDirectory(locationOfExtent, dataLength, fullPathParts)
             } else {
                 this.filesMapping.set(fullPath, {
-                    offset: locationOfExtent * 2048,
+                    location: locationOfExtent * SECTOR_SIZE,
                     size: dataLength,
+                    name: fullPath,
+                    flags,
                 })
             }
         }
     }
 
     async load() {
-        const rootDirectoryEntry = await this.bufferAt(17 * 2048 + 156, 34)
-        rootDirectoryEntry.getUint8() // Length of Directory Record
-        rootDirectoryEntry.getUint8() // Extended Attribute Record length
-        const rootLocation = rootDirectoryEntry.getUint32() // Location of extent LSB
-        rootDirectoryEntry.getUint32() // Location of extent MSB
-        const rootLength = rootDirectoryEntry.getUint32() // Data length LSB
-        rootDirectoryEntry.getUint32() // Data length MSB
+        const rootDirectoryEntry = await this.bufferAt(JULIET_SECTOR * SECTOR_SIZE + 156, 34)
+        rootDirectoryEntry.getUint8()
+        rootDirectoryEntry.getUint8()
+        const rootLocation = rootDirectoryEntry.getUint32() // LE
+        rootDirectoryEntry.getUint32() // BE
+        const rootLength = rootDirectoryEntry.getUint32() // LE
+        rootDirectoryEntry.getUint32() // BE
 
         await this.processDirectory(rootLocation, rootLength, [])
     }
@@ -135,7 +136,7 @@ export class Iso9660Reader {
             return null
         }
 
-        return this.readAt(entry.offset, entry.size)
+        return this.readAt(entry.location, entry.size)
     }
 
     getListing() {
