a3c97a7dd75526010db5de766ee610069acb8945
Adam Zambrzycki <adam@zambrzycki.net>
2025-06-22 07:34:19 +0200

    Don't cancel condition check if operand resolves to undefined. Fixes S53_0_PLATX

--- src/engine/types/condition.ts
+++ src/engine/types/condition.ts
@@ -28,48 +28,49 @@ export class Condition extends Type<ConditionDefinition> {
         const operand1 = this.engine.scripting.executeCallback(null, this.definition.OPERAND1)
         const operand2 = this.engine.scripting.executeCallback(null, this.definition.OPERAND2)
 
-        if (operand1 === undefined || operand2 === undefined) {
-            return false
-        }
-
         let result
-        try {
-            switch (this.definition.OPERATOR) {
-                case 'EQUAL':
-                    result = Compare.Equal(operand1, operand2)
-                    break
-                case 'NOTEQUAL':
-                    result = Compare.NotEqual(operand1, operand2)
-                    break
-                case 'LESS':
-                    result = Compare.Less(operand1, operand2)
-                    break
-                case 'GREATER':
-                    result = Compare.Greater(operand1, operand2)
-                    break
-                case 'LESSEQUAL':
-                    result = Compare.LessOrEqual(operand1, operand2)
-                    break
-                case 'GREATEREQUAL':
-                    result = Compare.GreaterOrEqual(operand1, operand2)
-                    break
+        if (operand1 !== undefined && operand2 !== undefined) {
+            try {
+                switch (this.definition.OPERATOR) {
+                    case 'EQUAL':
+                        result = Compare.Equal(operand1, operand2)
+                        break
+                    case 'NOTEQUAL':
+                        result = Compare.NotEqual(operand1, operand2)
+                        break
+                    case 'LESS':
+                        result = Compare.Less(operand1, operand2)
+                        break
+                    case 'GREATER':
+                        result = Compare.Greater(operand1, operand2)
+                        break
+                    case 'LESSEQUAL':
+                        result = Compare.LessOrEqual(operand1, operand2)
+                        break
+                    case 'GREATEREQUAL':
+                        result = Compare.GreaterOrEqual(operand1, operand2)
+                        break
+                }
+            } catch (err) {
+                if (err instanceof UnexpectedError) {
+                    console.error(
+                        'Condition details\n' + '\n' + '%cCondition:%c%O\n%cOperand1:%c%O\n%cOperand2:%c%O\n',
+                        'font-weight: bold',
+                        'font-weight: inherit',
+                        this,
+                        'font-weight: bold',
+                        'font-weight: inherit',
+                        operand1,
+                        'font-weight: bold',
+                        'font-weight: inherit',
+                        operand2
+                    )
+                }
+                throw err
             }
-        } catch (err) {
-            if (err instanceof UnexpectedError) {
-                console.error(
-                    'Condition details\n' + '\n' + '%cCondition:%c%O\n%cOperand1:%c%O\n%cOperand2:%c%O\n',
-                    'font-weight: bold',
-                    'font-weight: inherit',
-                    this,
-                    'font-weight: bold',
-                    'font-weight: inherit',
-                    operand1,
-                    'font-weight: bold',
-                    'font-weight: inherit',
-                    operand2
-                )
-            }
-            throw err
+        } else {
+            result = false
+            console.warn(`Condition ${this.name} has an operand that resolved to undefined. operand1=${operand1}, operand2=${operand2}`)
         }
 
         if (shouldSignal) {
