e6f99f9a5bc338f6f75ba53410f9829900e1dcf7
Adikso <Adikso@users.noreply.github.com>
2025-06-22 05:35:25 +0000

    deploy: a3c97a7dd75526010db5de766ee610069acb8945

--- main.js
+++ main.js
@@ -54576,37 +54576,40 @@ let Condition = (() => {
             CHECK(shouldSignal) {
                 const operand1 = this.engine.scripting.executeCallback(null, this.definition.OPERAND1);
                 const operand2 = this.engine.scripting.executeCallback(null, this.definition.OPERAND2);
-                if (operand1 === undefined || operand2 === undefined) {
-                    return false;
-                }
                 let result;
-                try {
-                    switch (this.definition.OPERATOR) {
-                        case 'EQUAL':
-                            result = types_1.Compare.Equal(operand1, operand2);
-                            break;
-                        case 'NOTEQUAL':
-                            result = types_1.Compare.NotEqual(operand1, operand2);
-                            break;
-                        case 'LESS':
-                            result = types_1.Compare.Less(operand1, operand2);
-                            break;
-                        case 'GREATER':
-                            result = types_1.Compare.Greater(operand1, operand2);
-                            break;
-                        case 'LESSEQUAL':
-                            result = types_1.Compare.LessOrEqual(operand1, operand2);
-                            break;
-                        case 'GREATEREQUAL':
-                            result = types_1.Compare.GreaterOrEqual(operand1, operand2);
-                            break;
+                if (operand1 !== undefined && operand2 !== undefined) {
+                    try {
+                        switch (this.definition.OPERATOR) {
+                            case 'EQUAL':
+                                result = types_1.Compare.Equal(operand1, operand2);
+                                break;
+                            case 'NOTEQUAL':
+                                result = types_1.Compare.NotEqual(operand1, operand2);
+                                break;
+                            case 'LESS':
+                                result = types_1.Compare.Less(operand1, operand2);
+                                break;
+                            case 'GREATER':
+                                result = types_1.Compare.Greater(operand1, operand2);
+                                break;
+                            case 'LESSEQUAL':
+                                result = types_1.Compare.LessOrEqual(operand1, operand2);
+                                break;
+                            case 'GREATEREQUAL':
+                                result = types_1.Compare.GreaterOrEqual(operand1, operand2);
+                                break;
+                        }
+                    }
+                    catch (err) {
+                        if (err instanceof errors_1.UnexpectedError) {
+                            console.error('Condition details\n' + '\n' + '%cCondition:%c%O\n%cOperand1:%c%O\n%cOperand2:%c%O\n', 'font-weight: bold', 'font-weight: inherit', this, 'font-weight: bold', 'font-weight: inherit', operand1, 'font-weight: bold', 'font-weight: inherit', operand2);
+                        }
+                        throw err;
                     }
                 }
-                catch (err) {
-                    if (err instanceof errors_1.UnexpectedError) {
-                        console.error('Condition details\n' + '\n' + '%cCondition:%c%O\n%cOperand1:%c%O\n%cOperand2:%c%O\n', 'font-weight: bold', 'font-weight: inherit', this, 'font-weight: bold', 'font-weight: inherit', operand1, 'font-weight: bold', 'font-weight: inherit', operand2);
-                    }
-                    throw err;
+                else {
+                    result = false;
+                    console.warn(`Condition ${this.name} has an operand that resolved to undefined. operand1=${operand1}, operand2=${operand2}`);
                 }
                 if (shouldSignal) {
                     if (result) {
