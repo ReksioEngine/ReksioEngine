d67e63c0a9893bd05ccd91f2a472ea6f34a7017f
x-Yoshida <69041080+x-Yoshida@users.noreply.github.com>
2025-07-20 12:37:44 +0200

    Initial implementation

--- src/engine/types/matrix.ts
+++ src/engine/types/matrix.ts
@@ -1,3 +1,8 @@
+import { Type, ValueType } from './index'
+import { MatrixDefinition } from '../../fileFormats/cnv/types'
+import { Engine } from '../index'
+import { assert, NotImplementedError } from '../../common/errors'
+import { method } from '../../common/types'
 // CALCENEMYMOVEDEST
 // CALCENEMYMOVEDIR
 // CANHEROGOTO
@@ -18,16 +23,27 @@
 // SETROW
 // TICK
 
-import { Type, ValueType } from './index'
-import { MatrixDefinition } from '../../fileFormats/cnv/types'
-import { Engine } from '../index'
-import { assert, NotImplementedError } from '../../common/errors'
-import { method } from '../../common/types'
+
+enum Field {
+    EMPTY = 0,
+    GROUND = 1,
+    STONE = 2,
+    DYNAMITE = 3,
+    WALL_WEAK = 4,
+    ENEMY = 5,
+    WALL_STRONG = 6,
+    DYNAMITE_FIRED = 7,
+    EXPLOSION = 8,
+    EXIT = 9,
+    MOLE = 99
+}
 
 export class Matrix extends ValueType<MatrixDefinition> {
     constructor(engine: Engine, parent: Type<any> | null, definition: MatrixDefinition) {
         super(engine, parent, definition, [])
     }
+
+    
 
     private width: number = 0;
     private height: number = 0;
@@ -35,13 +51,15 @@ export class Matrix extends ValueType<MatrixDefinition> {
     // SIZE[1] - Height
     init() {
         console.log(this.definition.SIZE)
+        this.width = this.definition.SIZE[0]
+        this.height = this.definition.SIZE[1]
         if(this.definition.SIZE[0]>0 && this.definition.SIZE[1]>0)
         for(let i=0;i<this.definition.SIZE[1];i++)
         {
-            this.value.push([])
+            // this.value.push([])
             for(let j=0;j<this.definition.SIZE[0];j++)
             {
-                this.value[i].push(0)
+                this.value.push(0)
             }
         }
     }
@@ -63,7 +81,11 @@ export class Matrix extends ValueType<MatrixDefinition> {
 
     @method()
     GET(...args:any[]){
-        throw new NotImplementedError()
+        console.log(args[0])
+        console.log("Got: " + this.value[args[0]])
+        return 
+        // return this.value[args[0]][args[1]] || 0; // Default to 0 if the cell is not set
+        // throw new NotImplementedError()
     }
 
     @method()
@@ -83,7 +105,21 @@ export class Matrix extends ValueType<MatrixDefinition> {
 
     @method()
     GETCELLSNO(...args:any[]){
-        throw new NotImplementedError()
+        let count = 0;
+        for(let i=0;i<this.value.length;i++)
+        {
+            if(this.value[i]==args[0])
+            {
+                count++;
+            }
+
+            
+        }
+
+        console.log("Count" + args[0] + ": " + count)
+        
+        return count;
+        // throw new NotImplementedError()
     }
 
     @method()
@@ -123,7 +159,11 @@ export class Matrix extends ValueType<MatrixDefinition> {
 
     @method()
     SET(...args:any[]){
-        throw new NotImplementedError()
+        const pos = args[0]
+        const value = args[1]
+
+        this.value[pos] = value;
+        // throw new NotImplementedError()
     }
 
     @method()
@@ -133,12 +173,13 @@ export class Matrix extends ValueType<MatrixDefinition> {
 
     @method()
     SETROW(...args:any[]){
-        throw new NotImplementedError()
+        this.value[args[0]*this.width] = args.slice(1)
+        // throw new NotImplementedError()
     }
     
     @method()
     TICK(...args:any[]){
-        throw new NotImplementedError()
+        // throw new NotImplementedError()
     }
     
 }
