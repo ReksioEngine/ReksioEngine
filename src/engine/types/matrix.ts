716efdf5606aef59c817d66dd721cd72ad662d09
x-Yoshida <69041080+x-Yoshida@users.noreply.github.com>
2025-07-21 16:39:21 +0200

    Implemented SETROW, GETCELLPOSX, GETCELLPOSY and changed to extend Type istead of ValueType

--- src/engine/types/matrix.ts
+++ src/engine/types/matrix.ts
@@ -23,7 +23,6 @@ import { method } from '../../common/types'
 // SETROW
 // TICK
 
-
 enum Field {
     EMPTY = 0,
     GROUND = 1,
@@ -35,151 +34,157 @@ enum Field {
     DYNAMITE_FIRED = 7,
     EXPLOSION = 8,
     EXIT = 9,
-    MOLE = 99
+    MOLE = 99,
 }
 
-export class Matrix extends ValueType<MatrixDefinition> {
+export class Matrix extends Type<MatrixDefinition> {
     constructor(engine: Engine, parent: Type<any> | null, definition: MatrixDefinition) {
-        super(engine, parent, definition, [])
+        super(engine, parent, definition)
     }
 
-    
+    private width: number = 0
+    private height: number = 0
+    private ticks: number = 0
+    private board: number[] = []
 
-    private width: number = 0;
-    private height: number = 0;
     // SIZE[0] - Width
     // SIZE[1] - Height
     init() {
         console.log(this.definition.SIZE)
         this.width = this.definition.SIZE[0]
         this.height = this.definition.SIZE[1]
-        if(this.definition.SIZE[0]>0 && this.definition.SIZE[1]>0)
-        for(let i=0;i<this.definition.SIZE[1];i++)
-        {
-            // this.value.push([])
-            for(let j=0;j<this.definition.SIZE[0];j++)
-            {
-                this.value.push(0)
-            }
+        if (this.width > 0 && this.height > 0) {
+            this.board = new Array(this.width * this.height).fill(Field.EMPTY)
         }
     }
 
     @method()
-    CALCENEMYMOVEDEST(...args:any[]){
+    CALCENEMYMOVEDEST(...args: any[]) {
+        // console.log(args)
         throw new NotImplementedError()
     }
 
     @method()
-    CALCENEMYMOVEDIR(...args:any[]){
+    CALCENEMYMOVEDIR(...args: any[]) {
+        // console.log(args)
         throw new NotImplementedError()
     }
 
     @method()
-    CANHEROGOTO(...args:any[]){
-        throw new NotImplementedError()
+    CANHEROGOTO(...args: any[]) {
+        console.log('CANHEROGOTO')
+        console.log(args)
+        // return true; // TODO: Implement this
     }
 
     @method()
-    GET(...args:any[]){
-        console.log(args[0])
-        console.log("Got: " + this.value[args[0]])
-        return 
-        // return this.value[args[0]][args[1]] || 0; // Default to 0 if the cell is not set
+    GET(index: number) {
+        return this.board[index]
         // throw new NotImplementedError()
     }
 
     @method()
-    GETCELLOFFSET(...args:any[]){
+    GETCELLOFFSET(...args: any[]) {
+        // console.log(args)
         throw new NotImplementedError()
     }
 
+    // BASEPOS - Offset of board in pixels from top left corner
     @method()
-    GETCELLPOSX(...args:any[]){
-        throw new NotImplementedError()
+    GETCELLPOSX(index: number) {
+        let posx: number = (index % this.width) * this.definition.CELLWIDTH + this.definition.BASEPOS[0]
+        // console.log(posx)
+        return posx
+        // throw new NotImplementedError()
     }
 
     @method()
-    GETCELLPOSY(...args:any[]){
-        throw new NotImplementedError()
+    GETCELLPOSY(index: number) {
+        let posy: number = Math.floor(index / this.width) * this.definition.CELLHEIGHT + this.definition.BASEPOS[1]
+        // console.log(posy)
+        return posy
+        // throw new NotImplementedError()
     }
 
     @method()
-    GETCELLSNO(...args:any[]){
-        let count = 0;
-        for(let i=0;i<this.value.length;i++)
-        {
-            if(this.value[i]==args[0])
-            {
-                count++;
+    GETCELLSNO(cellType: number) {
+        let count: number = 0
+        for (let i = 0; i < this.board.length; i++) {
+            if (this.board[i] == cellType) {
+                count++
             }
-
-            
         }
 
-        console.log("Count" + args[0] + ": " + count)
-        
-        return count;
+        console.log('Count ' + cellType + ': ' + count)
+
+        return count
+        // throw new NotImplementedError()
+    }
+
+    //Not used
+    @method()
+    GETFIELDPOSX(...args: any[]) {
+        // console.log(args)
+        throw new NotImplementedError()
+    }
+
+    //Not used
+    @method()
+    GETFIELDPOSY(...args: any[]) {
+        // console.log(args)
+        throw new NotImplementedError()
+    }
+
+    @method()
+    GETOFFSET(...args: any[]) {
+        throw new NotImplementedError()
+    }
+
+    @method()
+    ISGATEEMPTY(...args: any[]) {
+        throw new NotImplementedError()
+    }
+
+    @method()
+    ISINGATE(...args: any[]) {
+        // console.log(args)
+        throw new NotImplementedError()
+    }
+
+    @method()
+    MOVE(...args: any[]) {
+        throw new NotImplementedError()
+    }
+
+    @method()
+    NEXT(...args: any[]) {
+        // TODO: Implement this
+        return this.ticks + 4
         // throw new NotImplementedError()
     }
 
     @method()
-    GETFIELDPOSX(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    GETFIELDPOSY(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    GETOFFSET(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    ISGATEEMPTY(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    ISINGATE(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    MOVE(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    NEXT(...args:any[]){
-        throw new NotImplementedError()
-    }
-
-    @method()
-    SET(...args:any[]){
-        const pos = args[0]
-        const value = args[1]
-
-        this.value[pos] = value;
+    SET(index: number, value: number) {
+        this.board[index] = value
         // throw new NotImplementedError()
     }
 
     @method()
-    SETGATE(...args:any[]){
+    SETGATE(...args: any[]) {
+        // console.log(args)
         throw new NotImplementedError()
     }
 
     @method()
-    SETROW(...args:any[]){
-        this.value[args[0]*this.width] = args.slice(1)
+    SETROW(row: number, ...args: any[]) {
+        this.board.splice(row * this.width, this.width, ...args)
+        // this.board = this.board.map((v: any) => (v === Field.ENEMY ? Field.EMPTY : v)) // Cutout enemies for now
         // throw new NotImplementedError()
     }
-    
+
     @method()
-    TICK(...args:any[]){
+    TICK(...args: any[]) {
+        // console.log(args)
         // throw new NotImplementedError()
     }
-    
 }
